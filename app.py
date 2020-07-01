1fda69f8578586a73250eb09af2e1660765c7ed3
vzro <robertzero@msn.com>
2020-07-01 04:55:43 -0300

    adding first route

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Hello, world!</h1>'
+
 if __name__ == "__main__":
     app.run(debug=True, host='192.168.254.253', port=5000)
