d513db3b2d31de39fcde61588770c1583a5ff6b5
vzro <robertzero@msn.com>
2020-07-01 05:04:23 -0300

    adding the templates dir e the home.html page

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello, world!</h1>'
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='192.168.254.253', port=5000)
